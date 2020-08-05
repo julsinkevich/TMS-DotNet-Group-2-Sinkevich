75ed3146d95b74e15b27b282ef392e111b24f303
julsinkevich <julsinkevich@users.noreply.github.com>
2020-08-05 21:02:02 +0300

    fix: add model with key

---
+++ src/RecipesFinder_bot/Models/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipesFinder_bot.Models
+{
+    public class AppSettings
+    {
+        public static string Ur1 { get; set; } = "https://telegrambotapp.azurewebsites.net:443/{0}";
+        public static string Name { get; set; } = "RecipesFinder_bot";
+        public static string Key { get; set; } = "1148344323:AAEjZNPojEnCgeVdV4dBUO3ipXpKGDpwuMM";
+
+    }
+}
